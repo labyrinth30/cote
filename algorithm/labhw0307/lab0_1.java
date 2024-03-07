5128d318831a3d9ca050c2730d380e0c32b86216
younha00 <ayeon0@office.skhu.ac.kr>
2024-03-07 14:47:16 +0900

    ✨ Feat: 3/7일 강의와 lab 과제

---
+++ algorithm/labhw0307/lab0_1.java
@@ -0,0 +1,50 @@
+package algorithm.labhw0307;
+
+import java.util.Scanner;
+
+public class lab0_1 {
+    /**
+     * 사용자가 원하는 개수의 정수값들을 입력받아 평균값을 구하고, 그 정수값들 중에서 평균갑보다 큰 것이 총 몇 개인지 구하세요
+     *
+     * - 입력: 정수 개수(n), n개의 정수
+     * - 출력: 평균값, 평균값보다 큰 정수 개수
+     *
+     * - 실행 예:
+     *  lab0_1: 이윤하
+     *  10
+     *  1 2 3 4 5 6 7 8 9 10
+     *  5.5
+     *  5
+     */
+    public static void main(String[] args) {
+        System.out.println("lab0_1: 이윤하");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // 입력받은 수만큼의 크기를 가진 배열 생성
+        int[] arr = new int[n];
+
+        int sum = 0; // 초기화
+
+        // 입력받은 수만큼의 정수값을 배열에 저장하고 합을 구함
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        // 평균값 구하고 출력
+        double avg =  (double) sum / n;
+        System.out.println(avg);
+
+        int cnt = 0; // 초기화
+
+        // 평균값보다 큰 정수의 개수를 구함
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > avg) {
+                cnt++;
+            }
+        }
+        // 출력
+        System.out.println(cnt);
+        sc.close();
+    }
+}
---
+++ algorithm/labhw0307/lab0_2.java
@@ -0,0 +1,44 @@
+package algorithm.labhw0307;
+
+import java.util.Scanner;
+
+public class lab0_2 {
+    /**
+     * 다음과 같은 2가지 재귀 알고리즘을 작성하고, 사용자가 입력한 n을 매개변수로 이 두 메소드를 호출하여 결과를 얻는 프로그램을 작성하세요.
+     * - 양의 정수 n을 매개변수로 받아 1부터 n까지의 합을 구하여 리턴하는 메소드
+     * - 양의 정수 n을 매개변수로 받아 1부터 n까지의 정수를 출력하는 메소드
+     * - 입력: 정수값(n)
+     * - 출력: 1부터 n까지의 합과 정수리스트
+     * - 실행 예:
+     * lab0_2:이윤하
+     * 5                          // 정수값 n
+     * 15
+     * 1 2 3 4 5
+     */
+    public static void main(String[] args) {
+        System.out.println("lab0_2:이윤하");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+        print(n);
+        sc.close();
+    }
+    // 양의 정수 n을 매개변수로 받아 1부터 n까지의 합을 구하여 리턴하는 메소드
+    static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sum(n -1 );
+    }
+
+    // 양의 정수 n을 매개변수로 받아 1부터 n까지의 정수를 출력하는 메소드
+    static int print(int n) {
+        if (n == 1) {
+            System.out.print(n + " ");
+            return 1;
+        }
+        int result = print(n - 1);
+        System.out.print(n + " ");
+        return result;
+    }
+}
