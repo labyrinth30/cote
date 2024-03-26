945ad1e365a55e517a5db633a6737896d09203ea
younha00 <ayeon0@office.skhu.ac.kr>
2024-03-26 14:42:23 +0900

    ✨ Feat: 6차시 3/26 강의

--- algorithm/lab_hw_0314/lab0_3.java
+++ algorithm/lab_hw_0314/lab0_3.java
@@ -18,7 +18,7 @@ public class lab0_3 {
 // 정렬되지 않은 배열 array에서 원소를 순차검색하여 위지(인덱스)를 출력
         System.out.println(sequentialSearch(array, item));
 // 정렬된 배열 sortedArray에서 원소를 이진검색하여 위치(인덱스)를 출력
-        System.out.println(binarySearch(sortedArray, 0, array.length-1, item));
+        System.out.println(binarySearch(sortedArray, 0, sortedArray.length-1, item));
         scanner.close();
     }
 
@@ -33,7 +33,7 @@ public class lab0_3 {
         }
         return res;
     }
-    // 정렬된 배열 sortedArray에서 원소를 이진검색하여 위치(인덱스)를 리턴; 검색 실패시 1 리턴
+    // 정렬된 배열 sortedArray에서 원소를 이진검색하여 위치(인덱스)를 리턴; 검색 실패시 -1 리턴
     // 재귀알고리즘으로 구현해도 되고, 아니어도 됨. 재귀알고리즘으로 구현하지 않으려면 매개별수 from, to 필요 없음
     private static int binarySearch(int[] sortedArray, int from, int to, int item) {
         int res = -1;
